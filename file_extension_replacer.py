01cb07997bb3a5c690735f10f0da97bf77f9f03a
ms-afk <58094448+ms-afk@users.noreply.github.com>
2023-01-27 00:46:58 +0100

    clarification on walk order

--- file_extension_replacer.py
+++ file_extension_replacer.py
@@ -1,13 +1,14 @@
 #!/usr/bin/env python3
 import os, sys
 
-help_text = "File Extension Replacer\nusage: " + sys.argv[0] + " root oExt nExt\n\
+help_text = "File Extension Replacer\nusage: " + sys.argv[0] + " [OPTIONS] root oExt nExt\n\
 This utility replaces a chosen file extension with a new one.\n\
 The utility does so on all files in the selected directory, SUBDIRECTORIES INCLUDED.\n\
 Arguments meaning:\n\
 root	The path to the folder where this program will be executed (insert \".\" to use the current working directory)\n\
 oExt	The file extension to delete (e.g., \".txt\")\n\
 nExt	The file extension to replace the old one with (use \"\" to simply remove the files extension)\n\
+Options:\n\
 -h	Shows this help menu\n\
 --help	Shows this help menu"
 
@@ -19,7 +20,7 @@ if len(sys.argv) != 4:
 
 root, old_extension, new_extension = sys.argv[1:]
 
-for dirpath, dirnames, filenames in os.walk(root):
+for dirpath, dirnames, filenames in os.walk(root, topdown=True):
 	for filename in filenames:
 		if filename.endswith(old_extension):
 			full_path = os.path.join(dirpath, filename)
