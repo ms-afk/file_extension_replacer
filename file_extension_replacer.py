3531db6c2b44ea400bb35d16146efd9be7c8c725
ms-afk <58094448+ms-afk@users.noreply.github.com>
2023-01-27 01:21:30 +0100

    Added option, improvements

--- file_extension_replacer.py
+++ file_extension_replacer.py
@@ -1,6 +1,10 @@
 #!/usr/bin/env python3
 import os, sys
 
+#help menu
+
+options_description = {"-s" : "Disables the execution in the subdirectories", "-h" : "Shows this help menu", "--help" : "Shows this help menu"}
+
 help_text = "File Extension Replacer\nusage: " + sys.argv[0] + " [OPTIONS] root oExt nExt\n\
 This utility replaces a chosen file extension with a new one.\n\
 The utility does so on all files in the selected directory, SUBDIRECTORIES INCLUDED.\n\
@@ -8,17 +12,34 @@ Arguments meaning:\n\
  root	The path to the folder where this program will be executed (insert \".\" to use the current working directory)\n\
  oExt	The file extension to delete (e.g., \".txt\")\n\
  nExt	The file extension to replace the old one with (use \"\" to simply remove the files extension)\n\
-Options:\n\
- -h	Shows this help menu\n\
- --help	Shows this help menu"
+Options:"
 
-if "-h" in sys.argv or "--help" in sys.argv:
+def help():
 	print(help_text)
-	sys.exit(0)
-if len(sys.argv) != 4:
-	sys.exit("Invalid arguments!")
+	for opt in options_description:
+		print(" "+opt+"\t"+options_description[opt])
 
-root, old_extension, new_extension = sys.argv[1:]
+#command line arguments
+
+arguments = []
+options = []
+for el in sys.argv[1:]:
+	if el.startswith("-"):
+		options.append(el)
+	else:
+		arguments.append(el)
+for opt in options:
+	if not opt in options_description:
+		sys.exit("Invalid options! Use -h for help.")
+if "-h" in options or "--help" in options:
+        help()
+        sys.exit(0)
+if len(arguments) != 3:
+        sys.exit("Invalid arguments! Use -h for help.")
+
+#begin execution
+
+root, old_extension, new_extension = arguments
 
 for dirpath, dirnames, filenames in os.walk(root, topdown=True):
 	for filename in filenames:
@@ -27,3 +48,6 @@ for dirpath, dirnames, filenames in os.walk(root, topdown=True):
 			new_filename = filename.removesuffix(old_extension) + new_extension
 			new_full_path = os.path.join(dirpath, new_filename)
 			os.rename(full_path, new_full_path)
+	if "-s" in options:
+		#don't continue in subdirectories
+		break
